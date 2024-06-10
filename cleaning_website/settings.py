0b71b4d3e852d16709409c65516ea487cbb19482
EnesBrt <enes.barut@gmx.com>
2024-06-10 16:53:28 +0200

    chore: Update database configuration to use environment variables

--- cleaning_website/settings.py
+++ cleaning_website/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = "cleaning_website.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config("DB_HOST", default="localhost"),
-        "PORT": config("DB_PORT", default="5432"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
