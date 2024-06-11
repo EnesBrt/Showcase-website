9e8161916ecac1efef20fc404b2e6c5cbec8498f
EnesBrt <enes.barut@gmx.com>
2024-06-11 10:34:17 +0200

    chore: Update requirements.txt and add home page model

---
+++ showcase_website/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-62po%3^2i*4o%kmazn+*0d9=8pg!t7n-vuo+)3$ap+&8&90@!#"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
--- showcase_website/wsgi.py
+++ showcase_website/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cleaning_website project.
+WSGI config for showcase_website project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cleaning_website.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "showcase_website.settings.dev")
 
 application = get_wsgi_application()
