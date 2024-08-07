1e264e45d5b68323696a0c528678cc4538553a64
EnesBrt <enes.barut@gmx.com>
2024-08-07 15:48:44 +0200

    refactor: Add FooterText model for managing footer content

--- base/models.py
+++ base/models.py
@@ -77,3 +77,32 @@ class FormPage(AbstractEmailForm):
             addresses,
             self.from_address,
         )
+
+
+@register_snippet
+class FooterText(
+    DraftStateMixin,
+    RevisionMixin,
+    PreviewableMixin,
+    TranslatableMixin,
+    models.Model,
+):
+
+    body = RichTextField()
+
+    panels = [
+        FieldPanel("body"),
+        PublishingPanel(),
+    ]
+
+    def __str__(self):
+        return "Footer text"
+
+    def get_preview_template(self, request, mode_name):
+        return "base.html"
+
+    def get_preview_context(self, request, mode_name):
+        return {"footer_text": self.body}
+
+    class Meta(TranslatableMixin.Meta):
+        verbose_name_plural = "Footer Text"
