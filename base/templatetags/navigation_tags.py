bad3de263d7736fff62b497f89203fc6887e0078
EnesBrt <enes.barut@gmx.com>
2024-08-07 15:54:46 +0200

    refactor: Add FooterText model for managing footer content

--- base/templatetags/navigation_tags.py
+++ base/templatetags/navigation_tags.py
@@ -1,5 +1,7 @@
 from django import template
 
+from base.models import FooterText
+
 # import site:
 from wagtail.models import Site
 
@@ -15,3 +17,16 @@ def get_site_root(context):
 @register.simple_tag(takes_context=True)
 def get_contact_page(context):
     return Site.find_for_request(context["request"]).root_page.get_children().last()
+
+
+@register.inclusion_tag("base/includes/footer_text.html", takes_context=True)
+def get_footer_text(context):
+    footer_text = context.get("footer_text", "")
+
+    if not footer_text:
+        instance = FooterText.objects.filter(live=True).first()
+        footer_text = instance.body if instance else ""
+
+    return {
+        "footer_text": footer_text,
+    }
