856e835aeab7a6e73f073b0d3297f7c588d559f3
EnesBrt <enes.barut@gmx.com>
2024-08-14 22:52:06 +0200

    refactor: Update YouTubeHero component to use createRoot for rendering

--- home/static/js/index.js
+++ home/static/js/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import YouTubeHero from './components/App';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootElement = document.getElementById('react-app');
   if (rootElement) {
-    ReactDOM.render(
+    const root = createRoot(rootElement);
+    root.render(
       <React.StrictMode>
         <YouTubeHero />
-      </React.StrictMode>,
-      rootElement
+      </React.StrictMode>
     );
   } else {
     console.error('Root element not found');
